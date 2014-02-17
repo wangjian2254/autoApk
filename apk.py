373c98b23bb83650cdfc8c3575f32c256e5fc75c
王健 <wangjian2254@gmail.com>
2014-02-17 23:21:49 +0800

    自动下载

--- apk.py
+++ apk.py
@@ -2,12 +2,16 @@
 #author:u'王健'
 #Date: 14-2-15
 #Time: 下午2:15
+#import urllib.request
+import urllib
+import urllib2,HTMLParser
+
 __author__ = u'王健'
 
 import os
 import shutil
 import xml.dom.minidom
-
+import subprocess
 pluginxml = u'''<?xml version="1.0" encoding="UTF-8"?>
 <plugin-features>
     <version>1</version>
@@ -81,131 +85,283 @@ def Indent(dom, node, indent = 0):
 #    os.system(cmd_align)
 #    os.remove(signed_unalignedjar)
 
-def apkfile(apkname):
-    return '%s/%s'%(apkfiles,apkname)
+def apkfile(apkdir,apkname):
+    return '%s/%s/%s'%(apkfiles,apkdir,apkname)
 
 
 def cacheapkfile(apkname):
     return '%s/%s'%(cachefiles,apkname.replace('.apk','').replace('.','_'))
 
 def newapkfile(apkname):
-    return '%s/%s'%(newapkfiles,apkname)
-
-
-password = raw_input(u'输入签名密码')
-code = 'a4-s'
-codenum = 1010
+    return '%s/%s'%(apkfiles,apkname)
 
 apkfiles = 'apkfiles'
 newapkfiles = 'newapkfiles'
 cachefiles = 'cachefiles'
 
-apklist = []
-if os.path.exists(cachefiles):
-    shutil.rmtree(cachefiles)
-os.mkdir(cachefiles)
-if os.path.exists(newapkfiles):
-    shutil.rmtree(newapkfiles)
-os.mkdir(newapkfiles)
-for dirname in os.listdir('apkfiles'):
-    if dirname.find('.apk')<0:
-        continue
-    apklist.append(dirname)
-    print dirname
-    cmdExtract = r'java -jar lib/apktool.jar  d -f -s %s %s'% (apkfile(dirname),cacheapkfile(dirname))
-    #cmdExtract = r'java -jar lib/apktool.jar  d -f -s %s %s'% (apkfile(dirname),cacheapkfile(dirname))
-    os.system(cmdExtract)
+def autoApk():
+    password = raw_input(u'输入签名密码:\n')
+    code = 'a4-s'
+    codenum = 1010
 
 
 
-    #xmlstr = []
-    #for line in file('%s/AndroidManifest.xml'%newapkfile(dirname),'r'):
-    #    xmlstr.append(line)
-    androidManifest = xml.dom.minidom.parse('%s/AndroidManifest.xml'%cacheapkfile(dirname))
-    node = androidManifest.getElementsByTagName("manifest")[0]
-    package= node.getAttribute("package")
-    print package
-    node.setAttribute('android:sharedUserId','com.mogu3.allsharedid')
+    apklist = []
+    if os.path.exists(cachefiles):
+        shutil.rmtree(cachefiles)
+    os.mkdir(cachefiles)
+    if os.path.exists(newapkfiles):
+        shutil.rmtree(newapkfiles)
+    os.mkdir(newapkfiles)
+    for apkdir in os.listdir(apkfiles):
+        #if '.DS_Store' == apkdir:
+        #    continue
+        try:
+            apkdirlist = os.listdir('%s/%s'%(apkfiles,apkdir))
+        except:
+            continue
+        for dirname in apkdirlist:
+            if dirname[-4:]=='.apk':
+                apkdirlist.remove(dirname)
+                apkdirlist.insert(0,dirname)
+        if apkdirlist[0][-4:]!='.apk':
+            continue
+        appname = ''
+        for dirname in apkdirlist:
+            if dirname[-4:]!='.apk':
+                shutil.copyfile(apkfile(apkdir,dirname),'%s/%s'%(newapkfile(appname).encode('utf-8'),dirname))
+                continue
 
-    application = androidManifest.getElementsByTagName('application')[0]
-    appnameres = application.getAttribute('android:label').replace('@string/','')
-    appname = ''
-    stringxml = xml.dom.minidom.parse('%s/res/values/strings.xml'%cacheapkfile(dirname))
+            apklist.append(dirname)
+            print dirname
+            cmdExtract = r'java -jar lib/apktool.jar  d -f -s %s %s'% (apkfile(apkdir,dirname),cacheapkfile(dirname))
+            #cmdExtract = r'java -jar lib/apktool.jar  d -f -s %s %s'% (apkfile(dirname),cacheapkfile(dirname))
+            #os.system(cmdExtract)
+            p=subprocess.Popen(cmdExtract,shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            linelist = []
+            for line in p.stdout.readlines():
+                linelist.append(line)
+            if 'Exception' in ''.join(linelist):
+                print '失败了:%s'%apkdir
 
-    for s in stringxml.getElementsByTagName('string'):
-        if s.getAttribute('name') == appnameres:
-            appname = s.childNodes[0].nodeValue
-            print appname
-    if not appname:
-        continue
-    if os.path.exists(newapkfile(appname)):
-        shutil.rmtree(newapkfile(appname))
-    os.mkdir(newapkfile(appname))
+            #xmlstr = []
+            #for line in file('%s/AndroidManifest.xml'%newapkfile(dirname),'r'):
+            #    xmlstr.append(line)
+            androidManifest = xml.dom.minidom.parse('%s/AndroidManifest.xml'%cacheapkfile(dirname))
+            node = androidManifest.getElementsByTagName("manifest")[0]
+            package= node.getAttribute("package")
+            print package
+            node.setAttribute('android:sharedUserId','com.mogu3.allsharedid')
 
-    iconres = application.getAttribute('android:icon').replace('@drawable/','')
-    iconsrc0 = '%s/res/drawable/%s.png'%(cacheapkfile(dirname),iconres)
-    iconsrc1 = '%s/res/drawable-hdpi/%s.png'%(cacheapkfile(dirname),iconres)
-    iconsrc2 = '%s/res/drawable-xhdpi/%s.png'%(cacheapkfile(dirname),iconres)
-    iconsrc3 = '%s/res/drawable-xxhdpi/%s.png'%(cacheapkfile(dirname),iconres)
-    if os.path.exists(iconsrc0):
-        shutil.copyfile(iconsrc0,'%s/%s.png'%(newapkfile(appname),iconres))
-    elif os.path.exists(iconsrc1):
-        shutil.copyfile(iconsrc1,'%s/%s.png'%(newapkfile(appname),iconres))
-    elif os.path.exists(iconsrc2):
-        shutil.copyfile(iconsrc2,'%s/%s.png'%(newapkfile(appname),iconres))
-    elif os.path.exists(iconsrc3):
-        shutil.copyfile(iconsrc3,'%s/%s.png'%(newapkfile(appname),iconres))
+            application = androidManifest.getElementsByTagName('application')[0]
+            appnameres = application.getAttribute('android:label').replace('@string/','')
 
+            stringxml = xml.dom.minidom.parse('%s/res/values/strings.xml'%cacheapkfile(dirname))
 
+            for s in stringxml.getElementsByTagName('string'):
+                if s.getAttribute('name') == appnameres:
+                    appname = s.childNodes[0].nodeValue
+                    print appname
+            if not appname:
+                continue
+            if os.path.exists(newapkfile(appname)):
+                shutil.rmtree(newapkfile(appname))
+            os.mkdir(newapkfile(appname))
 
-
-    usersdk = androidManifest.getElementsByTagName('uses-sdk')
-    if len(usersdk):
-        node = usersdk[0]
-        sdkversion = node.getAttribute('minSdkVersion')
-    else:
-        sdkversion = u'所有版本'
-    print sdkversion
-    activity = androidManifest.getElementsByTagName('action')
-    mainclass = ''
-    for node in activity:
-        if node.getAttribute('android:name') == 'android.intent.action.MAIN':
-            mainclass = node.parentNode.parentNode.getAttribute('android:name')
-            if mainclass[0]=='.':
-                mainclass = '%s%s'%(package,mainclass)
-
-            node.setAttribute('android:name',mainclass)
-            categorylist = node.parentNode.getElementsByTagName('category')
-
-            for categorynode in categorylist:
-                if categorynode.getAttribute('android:name') =="android.intent.category.LAUNCHER":
-                    categorynode.setAttribute('android:name','android.intent.category.DEFAULT')
-
-    print mainclass
-    codenum+=1
-    pluginfile = file('%s/assets/plugin.xml'%cacheapkfile(dirname),'w')
-    pluginfile.write((pluginxml%(mainclass,appname,'%s%s'%(code,codenum))).encode('utf-8'))
-    pluginfile.close()
-
-    f=file('%s/AndroidManifest.xml'%cacheapkfile(dirname),'w')
-    import codecs
-    androidManifestcopy = androidManifest.cloneNode(True)
-    Indent(androidManifestcopy,androidManifestcopy.documentElement)
-    write = codecs.lookup('utf-8')[3](f)
-    androidManifestcopy.writexml(write,encoding='utf-8')
-    write.close()
-
-    unsignApk = r'%s/%s.apk'% (cachefiles, dirname.replace('.apk','').replace('.','_'))
-    cmdPack = r'java -jar lib/apktool.jar b -f %s %s'% (cacheapkfile(dirname),unsignApk)
-    os.system(cmdPack)
-
-    cmd_sign = r'jarsigner -verbose -keystore lib/MoGu3 -storepass %s -signedjar %s %s tianxingjian'% (password,'%s/%s'%(newapkfile(appname),dirname),unsignApk)
-    os.system(cmd_sign.encode('utf-8'))
+            iconres = application.getAttribute('android:icon').replace('@drawable/','')
+            iconsrc0 = '%s/res/drawable/%s.png'%(cacheapkfile(dirname),iconres)
+            iconsrc1 = '%s/res/drawable-hdpi/%s.png'%(cacheapkfile(dirname),iconres)
+            iconsrc2 = '%s/res/drawable-xhdpi/%s.png'%(cacheapkfile(dirname),iconres)
+            iconsrc3 = '%s/res/drawable-xxhdpi/%s.png'%(cacheapkfile(dirname),iconres)
+            if os.path.exists(iconsrc0):
+                shutil.copyfile(iconsrc0,'%s/%s.png'%(newapkfile(appname),iconres))
+            elif os.path.exists(iconsrc1):
+                shutil.copyfile(iconsrc1,'%s/%s.png'%(newapkfile(appname),iconres))
+            elif os.path.exists(iconsrc2):
+                shutil.copyfile(iconsrc2,'%s/%s.png'%(newapkfile(appname),iconres))
+            elif os.path.exists(iconsrc3):
+                shutil.copyfile(iconsrc3,'%s/%s.png'%(newapkfile(appname),iconres))
 
 
-if os.path.exists(cachefiles):
-    shutil.rmtree(cachefiles)
 
+
+            usersdk = androidManifest.getElementsByTagName('uses-sdk')
+            if len(usersdk):
+                node = usersdk[0]
+                sdkversion = node.getAttribute('minSdkVersion')
+            else:
+                sdkversion = u'所有版本'
+            print sdkversion
+            activity = androidManifest.getElementsByTagName('action')
+            mainclass = ''
+            for node in activity:
+                if node.getAttribute('android:name') == 'android.intent.action.MAIN':
+                    mainclass = node.parentNode.parentNode.getAttribute('android:name')
+                    if mainclass[0]=='.':
+                        mainclass = '%s%s'%(package,mainclass)
+
+                    node.setAttribute('android:name',mainclass)
+                    categorylist = node.parentNode.getElementsByTagName('category')
+
+                    for categorynode in categorylist:
+                        if categorynode.getAttribute('android:name') =="android.intent.category.LAUNCHER":
+                            categorynode.setAttribute('android:name','android.intent.category.DEFAULT')
+
+            print mainclass
+            codenum+=1
+            pluginfile = file('%s/assets/plugin.xml'%cacheapkfile(dirname),'w')
+            pluginfile.write((pluginxml%(mainclass,appname,'%s%s'%(code,codenum))).encode('utf-8'))
+            pluginfile.close()
+
+            f=file('%s/AndroidManifest.xml'%cacheapkfile(dirname),'w')
+            import codecs
+            androidManifestcopy = androidManifest.cloneNode(True)
+            Indent(androidManifestcopy,androidManifestcopy.documentElement)
+            write = codecs.lookup('utf-8')[3](f)
+            androidManifestcopy.writexml(write,encoding='utf-8')
+            write.close()
+
+            unsignApk = r'%s/%s_unsign.apk'% (cachefiles, dirname.replace('.apk','').replace('.','_'))
+            cmdPack = r'java -jar lib/apktool.jar b -f %s %s'% (cacheapkfile(dirname),unsignApk)
+            os.system(cmdPack)
+
+            cmd_sign = r'jarsigner -verbose -keystore lib/MoGu3 -storepass %s -signedjar %s %s tianxingjian'% (password,'%s/%s'%(newapkfile(appname),dirname),unsignApk)
+            os.system(cmd_sign.encode('utf-8'))
+
+
+    if os.path.exists(cachefiles):
+        shutil.rmtree(cachefiles)
+
+class ShowStructure(HTMLParser.HTMLParser):
+    def setlist(self,l):
+        self.alist = l
+        self.start = False
+    def handle_starttag(self, tag, attrs):
+        if tag == 'ul':
+            for att,val in attrs:
+                if att =='id' and val == 'iconList':
+                    self.start=True
+        if self.start and tag =='a':
+            for att , val in attrs:
+                if att == 'href' and val not in self.alist and val.find('zhushou360://')==-1:
+                    self.alist.append(val)
+        #self.alist.append(tag)
+    def handle_endtag(self, tag):
+        if tag=='ul':
+            self.start=False
+        #self.alist.pop()
+    def handle_data(self, data):
+        pass
+        #if data.strip():
+        #    for tag in self.alist:
+        #        sys.stdout.write('/'+tag)
+        #    sys.stdout.write(' >> %s/n' % data[:40].strip())
+
+class ApkPage(HTMLParser.HTMLParser):
+    def setlist(self):
+        self.infodict={'apkinfo':'','imglist':[]}
+        self.info = False
+        self.img = False
+    def handle_starttag(self, tag, attrs):
+        if tag == 'div':
+            for att,val in attrs:
+                if att =='class' and val == 'breif':
+                    self.info=True
+                if att =='class' and val =='overview':
+                    self.img = True
+        if self.img and tag =='img':
+            for att , val in attrs:
+                if att == 'src' and val not in self.infodict['imglist'] :
+                    self.infodict['imglist'].append(val)
+        #self.alist.append(tag)
+    def handle_endtag(self, tag):
+        if self.info and  tag=='div':
+            self.info=False
+        if self.img and tag == 'div':
+            self.img = False
+        #self.alist.pop()
+    def handle_data(self, data):
+        if self.info:
+            self.infodict['apkinfo']=data
+        #if data.strip():
+        #    for tag in self.alist:
+        #        sys.stdout.write('/'+tag)
+        #    sys.stdout.write(' >> %s/n' % data[:40].strip())
+
+
+def downloadApk():
+    urlbase = 'http://zhushou.360.cn'
+    urlstr='''
+    http://zhushou.360.cn/list/index/cid/2
+    http://zhushou.360.cn/list/index/cid/2?page=2
+    http://zhushou.360.cn/list/index/cid/2?page=3
+    http://zhushou.360.cn/list/index/cid/2?page=4
+    http://zhushou.360.cn/list/index/cid/2?page=5
+    '''
+    apkurllist=[]
+    for url in urlstr.split():
+        #fp = urllib.request.urlopen(url)
+        #mybytes = fp.read()
+
+        # note that Python3 does not read the html code as string
+        # but as html code bytearray, convert to string with
+        #html = mybytes.decode("utf8")      # 说明接收的数据是UTF-8格式(这样子可以解析和显示中文)
+
+        #fp.close()
+        html = urllib2.urlopen(url).read()
+        s = ShowStructure()
+        s.setlist(apkurllist)
+        s.feed(html)
+        #print html
+        #apklisthtml=xml.dom.minidom.parseString(html)
+        #for node in apklisthtml.getElementsByTagName('ui'):
+        #    if node.getAttribute('id')=='iconList':
+        #        for a in node.getElementsByTagName(a):
+        #            href = a.getAttribute('href')
+        #            if href not in apkurl:
+        #                apkurl.append(href)
+    for url in apkurllist[:1]:
+        #print url
+        html = urllib2.urlopen('%s%s'%(urlbase,url)).read()
+        scriptend = html.find('</script>')
+        apkstart = html[:scriptend].find("'downurl':")
+        apkend = html[apkstart:scriptend].find("'mkid'")+apkstart
+        apkurl = html[apkstart+10:apkend].split("'")[1]
+        print apkurl
+
+        #f = urllib2.urlopen(apkurl)
+        #data = f.read()
+        dirname = url.split('/')[-1]
+        filename = apkurl.split('/')[-1]
+        if os.path.exists('%s/%s'%(apkfiles,dirname)):
+            shutil.rmtree('%s/%s'%(apkfiles,dirname))
+        os.mkdir('%s/%s'%(apkfiles,dirname))
+        urllib.urlretrieve(apkurl,"%s/%s/%s"%(apkfiles,dirname,filename))
+        #with open("%s/%s/%s"%(apkfiles,dirname,filename), "wb") as code:
+        #    code.write(data)
+        linelist=[]
+        for line in html.split('\r\n'):
+            linelist.append(line.decode('utf-8'))
+        apkinfo = ApkPage()
+        apkinfo.setlist()
+        apkinfo.feed(''.join(linelist))
+        with open(u"%s/%s/简介.txt"%(apkfiles,dirname), "w") as code:
+            code.write(apkinfo.infodict['apkinfo'].encode('utf-8').replace('\n',''))
+        with open(u"%s/%s/应用地址.txt"%(apkfiles,dirname), "w") as code:
+            code.write(url)
+        for i,imgurl in enumerate( apkinfo.infodict['imglist']):
+            f = urllib2.urlopen(imgurl)
+            data = f.read()
+            filename = imgurl.split('.')[-1]
+            with open("%s/%s/%s.%s"%(apkfiles,dirname,i,filename), "wb") as code:
+                code.write(data)
+
+
+
+
+
+downloadApk()
+autoApk()
 #
 #apkName = 'ApkTest.apk'
 #easyName = apkName.split('.apk')[0]
