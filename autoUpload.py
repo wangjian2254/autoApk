a883316eab5b16d187a8f6afe94c48e39a81c8cb
王健 <wangjian2254@gmail.com>
2014-03-11 09:42:01 +0800

    优化上传模块

--- autoUpload.py
+++ autoUpload.py
@@ -110,6 +110,7 @@ def encode_multipart_data(data, files):
                'Referer':'http://mogu-mmggoo.appsp0t.com/PluginUploadApkScript',
                'DNT':'1',
                'Connection':'keep-alive',
+               'Accept-Encoding':' deflate',
                'Accept-Language':'zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3',
                'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                'User-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:27.0) Gecko/20100101 Firefox/27.0',
@@ -212,9 +213,9 @@ def run_upload(options, paths):
 
 
 def uploadApkOnly():
-    from poster.encode import multipart_encode
-    from poster.streaminghttp import register_openers
-    import urllib2
+    # from poster.encode import multipart_encode
+    # from poster.streaminghttp import register_openers
+    # import urllib2
     i = 1
     for dirname in os.listdir(newapkfiles):
         try:
@@ -223,26 +224,26 @@ def uploadApkOnly():
             continue
         print 'no. %s'%i
         i+=1
-        data = {'name':dirname}
-        files = {}
+        data = {'appcode':'com.mfp.frozen.playgame.disney'}
+        files = {'file':'4.jpg'}
         apkdir = ''
         num = 1
-        for filename in apkdirlist:
-            if filename[-4:] == '.apk':
-                apkdir = '%s/%s/%s'%(newapkfiles, dirname, filename)
-            if filename.find('package.txt')>=0:
-                data['appcode']=open('%s/%s/%s'%(newapkfiles, dirname, filename),'r').read()
+        # for filename in apkdirlist:
+        #     if filename[-4:] == '.apk':
+        #         apkdir = '%s/%s/%s'%(newapkfiles, dirname, filename)
+        #     if filename.find('package.txt')>=0:
+        #         data['appcode']=open('%s/%s/%s'%(newapkfiles, dirname, filename),'r').read()
 
 
-        result = send_post(uploadurl2, data, files)
+        result = send_post(uploadurl2, data, {})
         r = json.loads(result)
-        register_openers()
+        # register_openers()
         if r.get('status', 0) == 200:
             #datagen, headers = multipart_encode({"file": open(apkdir, "rb")})
             #request = urllib2.Request(str(r.get('upload_url')), datagen, headers)
             #print urllib2.urlopen(request).read()
             #print send_post('http://mogu-mmggoo.appspot.com/_ah/upload/?id=6416702615060480&pluginid=4896155343257600/AMmfu6b-snciTp0aUc_hjzq0zjfDzUj2d1JCVMzfM4CAABLjMZZu36kGMSN7LMK2Rg-i1ql7_tF5Qos0RAidXR_JnrGoyQfdkxIIFxK58LwccI_UhAzyUHUHac1cImaH3gJp0QtNcaqjU3mmtmVIqm9JZsSEt-ca7oOEOz7qGIc6GoIomHCrzEA/ALBNUaYAAAAAUx3QZzF6Yb38tzMLRxWvq22S3Sc0sHUF/',{},{'file': apkdir})
-            print send_post(str(r.get('upload_url')),{},{'file': apkdir})
+            print send_post(str(r.get('upload_url')),{},files)
             print 'success:%s'%dirname
 
         else:
